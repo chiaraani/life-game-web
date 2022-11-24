2a368f6ecdd180eedd77adbddc79a05a2109bab9
Chiara Nuñez <chiaraani@gmail.com>
2022-11-24 22:22:16 +0100

    Fix Grid helpers

--- app/helpers/grids_helper.rb
+++ app/helpers/grids_helper.rb
@@ -2,21 +2,7 @@
 
 # Helpers for grid views
 module GridsHelper
-  def min_and_max(attribute)
-    validator = GridData.validators_on(attribute).find do |v|
-      v.is_a? ActiveModel::Validations::NumericalityValidator
-    end
-
-    range = validator.instance_variable_get('@options')[:in]
-
-    { min: range.min, max: range.max }
-  end
-
-  def type_of(attribute)
-    Grid.attribute_types[attribute.to_s].to_s.split(':')[-2].downcase.to_sym
-  end
-
-  def step(attribute)
-    { step: type_of(attribute) == :integer ? 1 : 'any' }
+  def step_of(model, attribute)
+    { step: model.type_of(attribute) == :integer ? 1 : 'any' }
   end
 end
--- spec/helpers/grids_helper_spec.rb
+++ spec/helpers/grids_helper_spec.rb
@@ -3,31 +3,14 @@
 require 'rails_helper'
 
 RSpec.describe GridsHelper, type: :helper do
-  describe '#min_and_max' do
-    it 'returns min and max of attribute' do
-      expect(helper.min_and_max(:rows)).to eq({ min: 1, max: 50 })
-    end
-  end
-
-  describe '#type_of' do
-    shared_examples 'type' do |attribute, type|
-      it 'returns type of attribute' do
-        expect(helper.type_of(attribute)).to eq(type)
-      end
-    end
-
-    include_examples 'type', :rows, :integer
-    include_examples 'type', :phase_duration, :float
-  end
-
-  describe '#step' do
-    shared_examples 'step' do |attribute, type, step|
+  describe '#step_of' do
+    shared_examples 'step of' do |attribute, type, step|
       it "returns #{type} step" do
-        expect(helper.step(attribute)).to eq({ step: })
+        expect(helper.step_of(GridData, attribute)).to eq({ step: })
       end
     end
 
-    include_examples 'step', :rows, :integer, 1
-    include_examples 'step', :phase_duration, :float, 'any'
+    include_examples 'step of', :rows, :integer, 1
+    include_examples 'step of', :phase_duration, :float, 'any'
   end
 end
